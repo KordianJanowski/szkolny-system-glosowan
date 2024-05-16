baa269308748219d3b41485f160c777f9221049f
Kordian Janowski <kordianj1227@gmail.com>
2024-05-17 00:07:38 +0200

    prod

--- frontend/src/pages/sign-in.tsx
+++ frontend/src/pages/sign-in.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SignInForm } from '../features';
 import { checkIfUserLogged } from '../utils/helpers/redirect';
+import { VotingsService } from '../utils/api';
 
 const SignIn: React.FC = () => {
   checkIfUserLogged()
+
+  const getVotings = async () => {
+    const votings =  await VotingsService.findAll()
+
+    if(!votings) return
+    console.log(votings)
+  }
+
+  useEffect(() => {
+    getVotings()
+  }, []);
 
   return (
     <div className='flex items-center justify-center h-screen bg-gray-200'>
